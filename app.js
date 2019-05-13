3e34130b31afd4a31081d967daf785b0c062deed
Amos Welike <amoswelike@gmail.com>
2019-05-13 19:52:18 +0300

    add to data structure with validated fields

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import db from './db/db';
 // Set up the express app
 const app = express();
+// Parse incoming requests data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 // get all todos
 app.get('/api/v1/todos', (req, res) => {
   res.status(200).send({
@@ -10,6 +14,33 @@ app.get('/api/v1/todos', (req, res) => {
     todos: db
   })
 });
+// add a todo to the databsse
+app.post('/api/v1/todos', (req, res) => {
+    if(!req.body.title) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'title is required'
+      });
+    } else if(!req.body.description) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'description is required'
+      });
+    }
+
+   const todo = {
+     id: db.length + 1,
+     title: req.body.title,
+     description: req.body.description
+   }
+   db.push(todo);
+   return res.status(201).send({
+     success: 'true',
+     message: 'todo added successfully',
+     todo
+   })
+  });
+
 const PORT = 5000;
 
 app.listen(PORT, () => {
